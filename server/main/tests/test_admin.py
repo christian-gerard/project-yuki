4396212d4e760bbe00ec5030193700b2f5d341f8
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-11-14 12:15:46 -0700

    Adds Testing & Cleans Up URL Files

---
+++ server/main/tests/test_admin.py
@@ -0,0 +1,19 @@
+"""
+Tests for Admin Page
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+class AdminSiteTests(TestCase):
+    """Test for Django admin"""
+
+    def setUp(self):
+        """Set Up"""
+        pass
+
+    def test_tests_folder(self):
+        """Testing Django Testing"""
+        pass
