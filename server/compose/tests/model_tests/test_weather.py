4fe5c8c2dbd6153e46f9c5299112eec4d242e6a2
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-11-18 17:02:49 -0700

    Initializes model & testing

---
+++ server/compose/tests/model_tests/test_weather.py
@@ -0,0 +1,19 @@
+"""
+Test for Weather Model
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+class WeatherModelTests(TestCase):
+    """Test for Weather Model"""
+
+    def setUp(self):
+        """Set Up"""
+        pass
+
+    def test_tests_folder(self):
+        """Testing Django Testing"""
+        pass
