e7fb4447a53a12b118efa693678ed7856c17aaf2
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-11-22 09:44:15 -0700

    Weather Station Name

--- server/compose/models/WeatherStation.py
+++ server/compose/models/WeatherStation.py
@@ -6,7 +6,7 @@ from django.db import models
 class WeatherStation(models.Model):
     """Fields for Weather Station"""
     station_id = models.IntegerField()
-    name = models.CharField()
+    name = models.CharField(max_length=255)
     elevation = models.FloatField()
     latitude = models.FloatField()
     longitude = models.FloatField()
