f492973edda8793c4d9bf75b2a42997b437ddc0d
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-11-21 19:20:12 -0700

    Adding the models to WeatherData

--- server/compose/models/WeatherModel.py
+++ server/compose/models/WeatherModel.py
@@ -7,24 +7,21 @@ from django.db import models
 class WeatherData(models.Model):
     """ Weather Data Model """
 
-    weatherStation = models.DecimalField()
-    temperature = models.DecimalField()
+    time_stamp = models.DateTimeField()
+    air_temp = models.FloatField()
+    relative_humidity = models.FloatField()
+    wind_speed = models.FloatField()
+    wind_direction = models.CharField()
+    wind_gust = models.FloatField()
+    solar_radiation = models.FloatField()
+    outgoing_radiation_lw = models.FloatField()
+    incoming_radiation_lw = models.FloatField()
+    snow_depth = models.FloatField()
+    surface_temp = models.FloatField()
+    volt = models.FloatField()
+    wind_cardinal_direction = models.FloatField()
+    dew_point_temperature = models.FloatField()
 
     def __str__(self):
         return self.weatherStation
 
-
-    # 'date_time': ['2024-11-21T06:00:00Z'],
-    # 'air_temp_set_1': [31.52],
-    # 'relative_humidity_set_1': [34.48],
-    # 'wind_speed_set_1': [0.53],
-    # 'wind_direction_set_1': [48.86],
-    # 'wind_gust_set_1': [3.02],
-    # 'solar_radiation_set_1': [0.0],
-    # 'outgoing_radiation_sw_set_1': [5.33],
-    # 'incoming_radiation_lw_set_1': [258.67],
-    # 'snow_depth_set_1': [16.0],
-    # 'surface_temp_set_1': [16.85],
-    # 'volt_set_1': [12.03],
-    # 'wind_cardinal_direction_set_1d': ['NE'],
-    # 'dew_point_temperature_set_1d': [6.46]},
