889221307f1001a49fd51327f5620d403ea08b9d
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-11-22 10:27:46 -0700

    Updating

--- server/compose/models/WeatherStation.py
+++ server/compose/models/WeatherStation.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class WeatherStation(models.Model):
     """Fields for Weather Station"""
+
     station_id = models.IntegerField()
     name = models.CharField(max_length=255)
     elevation = models.FloatField()
