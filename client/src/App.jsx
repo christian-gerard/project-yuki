41cd7599b1f4becef3d5e4d530e0095e6e80b9a9
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-12-04 16:53:01 -0700

    Updating the and implementing styled divs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,19 +2,30 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import styled from 'styled-components'
+
+const Header = styled.div`
+  background-color: blue;
+  color: white;
+  padding: 10px;
+`;
+
+const AppWindow = styled.div`
+  border: black 1 solid;
+  width: 100%;
+  height: 100%;
+  background-color:blue;
+`
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <p>Project Yuki</p>
-      </div>
-
-
-    </>
-  )
+    <AppWindow>
+      <Header>
+        <p>Deep</p>
+        <p>Testing Names</p>
+      </Header>
+    </AppWindow>  )
 }
 
 export default App
