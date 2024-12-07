e54fe44d7939846a3d5ca4485bf1563d03803fa7
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-12-06 19:32:28 -0700

    Updating my token

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,12 @@
 import SWE from '../data-visualization/SWE'
+import SnowDepth from '../data-visualization/SnowDepth'
 
 function Dashboard() {
 
     return(
         <>
             <SWE/>
+            <SnowDepth/>
         </>
     )
 }
