32cb1c2ff2db1b4223ecae7bb7543d5ba32c437e
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-12-11 20:13:21 -0700

    Updating

--- client/src/data-visualization/Rating.jsx
+++ client/src/data-visualization/Rating.jsx
@@ -7,7 +7,6 @@ function Rating() {
   const Main = styled.div`
       width:98%;
       height:40%;
-      border: 1px solid black;
       overflow: hidden;
       white-space: nowrap;
       display:flex;
@@ -26,79 +25,36 @@ function Rating() {
       white-space: nowrap;
       font-size: 1.5em;
   `
-  const chartRef = useRef(null);
+  const StokeMeter = styled.div`
+      display: flex;
+      flex-direction: column;
+      height: 90%;
+      width:80%;
+      border: 1px solid black;
+      justify-content: flex-end;
+ `
+  const Meter = styled.div`
+      height:20%;
+      width:100%;
+      background-color: var(--light-cyan);
+      border: black 1px solid;
+  `
 
-  const data = [{
-
-  }]
 
   useEffect(() => {
-    const width = 500;
-    const height = 300;
-    const margin = { top: 20, right: 20, bottom: 50, left: 50 };
 
-    // Remove any existing SVG
-    d3.select(chartRef.current).select("svg").remove();
-
-    // Create SVG container
-    const svg = d3
-      .select(chartRef.current)
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-    // Create a stack
-    const stack = d3.stack().keys(keys)(data);
-
-    // Scales
-    const x = d3
-      .scaleBand()
-      .domain(data.map((d) => d.group))
-      .range([0, width])
-      .padding(0.1);
-
-    const y = d3
-      .scaleLinear()
-      .domain([0, d3.max(stack[stack.length - 1], (d) => d[1])])
-      .nice()
-      .range([height, 0]);
-
-    const colorScale = d3.scaleOrdinal().domain(keys).range(colors);
-
-    // Axes
-    svg
-      .append("g")
-      .attr("transform", `translate(0, ${height})`)
-      .call(d3.axisBottom(x));
-
-    svg.append("g").call(d3.axisLeft(y));
-
-    // Add bars
-    svg
-      .selectAll(".layer")
-      .data(stack)
-      .enter()
-      .append("g")
-      .attr("class", "layer")
-      .attr("fill", (d) => colorScale(d.key))
-      .selectAll("rect")
-      .data((d) => d)
-      .enter()
-      .append("rect")
-      .attr("x", (d) => x(d.data.group))
-      .attr("y", (d) => y(d[1]))
-      .attr("height", (d) => y(d[0]) - y(d[1]))
-      .attr("width", x.bandwidth());
-       }, [])
+  }, [])
 
   return (
     <Main>
 
       <RatingChart>
-        <p>Charts</p>
-        <div ref={chartRef}></div>
+        <p>Stoke Meter</p>
+        <StokeMeter>
+          <Meter/>
+          <Meter/>
+          <Meter/>
+        </StokeMeter>
       </RatingChart>
 
       <RatingReasons>
